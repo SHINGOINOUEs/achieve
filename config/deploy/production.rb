3af04fd9649493765e67bf5467dd050d2f37fdd4
shingo inoue <zhangmoutou@yahoo.co.jp>
2023-03-09 14:12:38 +0900

    deploy error

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '172.31.4.147', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: /Users/inoueshingo/.ssh/id_rsa
+server '35.79.245.255', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/inoueshingo/.ssh/id_rsa'
